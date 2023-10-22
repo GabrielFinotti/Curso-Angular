80a985f2f8333dedb239a1461a9f41a72096de41
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-10-21 21:38:43 -0300

    update data binding
    Aprendendo a utilizar Event Binding e a usar $event para receber todos os tipos de informação possível da tag/elemento html!

--- meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -8,12 +8,22 @@ import { Component } from '@angular/core';
 export class DataBindingComponent {
   public nome: string = 'Dener';
   public idade: number = 29;
-
   public checkedDisable: boolean = true;
+  public position: { x: number; y: number } = { x: 0, y: 0 };
 
   constructor() {
     setTimeout(() => {
       this.checkedDisable = false;
     }, 3000);
+  }
+
+  public alertaInfo(valor: MouseEvent) {
+    console.log(valor);
+  }
+
+  public mouseMoveTest(valor: MouseEvent) {
+    console.log(valor);
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
   }
 }
