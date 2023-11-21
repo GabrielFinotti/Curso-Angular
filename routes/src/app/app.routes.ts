ee84270bd9d17311475f620731a2ef5a9d14c1a0
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-21 19:20:44 -0300

    Routes
    Aprendendo a utilizar rotas

---
+++ routes/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'sobre', component: SobreComponent },
+  { path: '**', component: NotFoundComponent },
+];
---
+++ routes/src/app/pages/sobre/sobre.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sobre',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sobre.component.html',
+  styleUrl: './sobre.component.scss'
+})
+export class SobreComponent {
+
+}
