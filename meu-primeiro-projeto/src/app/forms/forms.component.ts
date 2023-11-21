b6afc420e92490e6cd86cdace075d14277e665d6
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-21 11:41:35 -0300

    ngForm
    Aprendendo a utilizar select

--- meu-primeiro-projeto/src/app/forms/forms.component.ts
+++ meu-primeiro-projeto/src/app/forms/forms.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent {
+  public listComidas!: Array<{ comida: string; preco: number }>;
 
+  constructor() {
+    this.listComidas = [
+      { comida: 'X-Salada', preco: 10 },
+      { comida: 'X-Bacon', preco: 11 },
+      { comida: 'Coxinha', preco: 12 },
+    ];
+  }
 }
