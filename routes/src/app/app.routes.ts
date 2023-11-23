99f86da3bfb1924718e50141060a51632fa0411f
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-23 13:24:06 -0300

    Rotas
    Criando rotas filhas!

--- routes/src/app/app.routes.ts
+++ routes/src/app/app.routes.ts
@@ -5,7 +5,10 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'sobre', component: SobreComponent },
-  { path: 'sobre/:id/:username', component: SobreComponent },
+  {
+    path: 'sobre',
+    component: SobreComponent,
+    children: [{ path: ':id/:username', component: SobreComponent }],
+  },
   { path: '**', component: NotFoundComponent },
 ];
--- routes/src/app/pages/sobre/sobre.component.ts
+++ routes/src/app/pages/sobre/sobre.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
@@ -10,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './sobre.component.scss',
 })
 export class SobreComponent implements OnInit {
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((res) =>
@@ -18,5 +18,12 @@ export class SobreComponent implements OnInit {
     );
 
     this.activatedRoute.queryParams.subscribe((res) => console.log(res));
+
+    setInterval(() => {
+      // Esse redireciona sem recarregar a página
+      // this.router.navigate(['404']);
+      // Esse redireciona recarregando a página por completa
+      // this.router.navigateByUrl('404')
+    },5000)
   }
 }
